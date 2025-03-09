93d233e58fb54c2e06a92625c2f629d90f765211
ianmwendwa11 <ian.musyoki12@gmail.com>
2025-03-09 14:50:35 +0300

    assign

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Implementing mondayWork function
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Implementing wrapAdjective function
+  const wrapAdjective = function(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  };
+  
